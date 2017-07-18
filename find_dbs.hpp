68a36e8d4c0954b08eacb5bb92fc2b9a59bd6421
Girish Gopalakrishna pillai <nohupped@users.noreply.github.com>
2017-07-18 11:31:44 +0800

    Delete find_dbs.hpp

--- find_dbs.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef LEVELDB_FIND_DBS_HPP
-#define LEVELDB_FIND_DBS_HPP
-#include <vector>
-#include <string>
-std::vector<std::basic_string<char> > getDirs(char* path);
-char* get_absolute_path(char* path, char* dname);
-
-#endif //LEVELDB_FIND_DBS_H
